82723f971268c1af2d10cd03d9b791bfba51f040
Fernando <oficialfcsj@gmail.com>
2023-10-16 02:17:22 -0400

    build: Aula 5 - Validações e Deploy da API.

--- src/main/java/br/com/estudojava/todolist/task/TaskController.java
+++ src/main/java/br/com/estudojava/todolist/task/TaskController.java
@@ -40,6 +40,7 @@ public class TaskController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser antes da data final.");
         }
 
+        
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
@@ -52,10 +53,23 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public void update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
         var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Usuário não tem permissão para alterar essa tarefa");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
-        this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 
 }
--- src/main/java/br/com/estudojava/todolist/task/TaskModel.java
+++ src/main/java/br/com/estudojava/todolist/task/TaskModel.java
@@ -14,9 +14,9 @@ import lombok.Data;
 @Data
 @Entity(name = "tb_tasks")
 public class TaskModel {
-    
+
     @Id
-    @GeneratedValue(generator= "UUID")
+    @GeneratedValue(generator = "UUID")
     private UUID id;
     private String description;
     @Column(length = 50)
@@ -24,10 +24,17 @@ public class TaskModel {
     private LocalDateTime startAt;
     private LocalDateTime endAt;
     private String priority;
-    
+
     private UUID idUser;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo title deve conter no máximo 50 caracteres");
+        }
+        this.title = title;
+    }
 
 }
